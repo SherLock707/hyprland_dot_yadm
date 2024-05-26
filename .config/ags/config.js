cc2a978c6c5d8ccd0ad64c3480d64568e5bf3de1
Kiran George <kirangeorge1995@gmail.com>
2024-05-26 12:54:16 +0530

    Waybar update

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -3,11 +3,13 @@ import GLib from 'gi://GLib';
 import App from 'resource:///com/github/Aylur/ags/app.js'
 import userOptions from './modules/.configuration/user_options.js';
 import Overview from './modules/overview/main.js';
+// import AudioContent from "./modules/audio/index.js";
+// import OSD from './modules/osd/OSD.js';
+// import { forMonitors } from './modules/utils.js';
 
 const COMPILED_STYLE_DIR = `${GLib.get_user_config_dir()}/ags/user/`
 
 async function applyStyle() {
-
     App.resetCss();
     App.applyCss(`${COMPILED_STYLE_DIR}/style.css`);
     console.log('[LOG] Styles loaded')
@@ -15,9 +17,12 @@ async function applyStyle() {
 applyStyle().catch(print);
 
 const Windows = () => [
-    Overview()
+    Overview(),
+    // AudioContent()
+    // OSD(),
 ];
 const CLOSE_ANIM_TIME = 210;
+
 App.config({
     css: `${COMPILED_STYLE_DIR}/style.css`,
     stackTraceOnError: true,
