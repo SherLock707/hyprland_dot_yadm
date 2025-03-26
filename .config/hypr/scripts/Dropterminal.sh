ba68af122b46e54edf4f400fe0ca60a267bef4a8
Kiran George <kirangeorge1995@gmail.com>
2025-03-26 21:14:52 +0530

    commit - 2025-03-26_21-14-52

--- .config/hypr/scripts/Dropterminal.sh
+++ .config/hypr/scripts/Dropterminal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPECIAL_WS="special"
+SPECIAL_WS="special:scratchpad"
 FOOT_CLASS="foot-dropterminal"
 
 # Get the current workspace
@@ -9,15 +9,16 @@ CURRENT_WS=$(hyprctl activeworkspace -j | jq -r '.name')
 # Check if foot-dropterminal exists
 if hyprctl clients -j | jq -e --arg CLASS "$FOOT_CLASS" 'any(.[]; .class == $CLASS)'; then
     # If exists, check if it's in special workspace
-    if hyprctl clients -j | jq -e --arg CLASS "$FOOT_CLASS" 'any(.[]; .class == $CLASS and .workspace.name == "special:special")'; then
+    if hyprctl clients -j | jq -e --arg CLASS "$FOOT_CLASS" 'any(.[]; .class == $CLASS and .workspace.name == "special:scratchpad")'; then
         # Move back to current workspace and focus
         hyprctl dispatch movetoworkspace "$CURRENT_WS",class:$FOOT_CLASS
         hyprctl dispatch focuswindow class:$FOOT_CLASS
+        hyprctl dispatch pin class:$FOOT_CLASS
     else
         # Move to special workspace silently
         hyprctl dispatch movetoworkspacesilent $SPECIAL_WS, class:$FOOT_CLASS
     fi
 else
     # If not running, start a new instance
-    hyprctl dispatch exec "[float; move 15% 5%; size 70% 60%; special] foot --app-id foot-dropterminal"
+    hyprctl dispatch exec "[float; pin; move 15% 5%; size 70% 60%; special] foot --app-id foot-dropterminal"
 fi
