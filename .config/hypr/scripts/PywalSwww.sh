a613bb8c5a9ed5e7824a9f89448e79c17b3de9c4
Kiran George <kirangeorge1995@gmail.com>
2025-06-03 12:37:12 +0530

    commit - 2025-06-03_12-37-12

--- .config/hypr/scripts/PywalSwww.sh
+++ .config/hypr/scripts/PywalSwww.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
 
-# Define the path to the swww cache directory
-# cache_dir="$HOME/.cache/swww/"
-
-# # Get a list of monitor outputs
-# monitor_outputs=($(ls "$cache_dir"))
-
-# # Loop through monitor outputs
-# for output in "${monitor_outputs[@]}"; do
-#     # Construct the full path to the cache file
-#     cache_file="$cache_dir$output"
-
-#     # Check if the cache file exists for the current monitor output
-#     if [ -f "$cache_file" ]; then
-#         # Get the wallpaper path from the cache file
-#         # wallpaper_path=$(cat "$cache_file")
-#         # wallpaper_path=$(tail -n 1 "$cache_file")
-#         wallpaper_path=$(swww query | head -n 1 | grep -oP 'image: \K.*')
-
-#         # Copy the wallpaper to the location Rofi can access
-#         ln -sf "$wallpaper_path" "$HOME/.config/rofi/.current_wallpaper"
-#         convert "$wallpaper_path" -resize 40% -filter Gaussian -blur 0x8 $HOME/.config/rofi/.current_wallpaper_blur.png &
-
-#         break  # Exit the loop after processing the first found monitor output
-#     fi
-# done
-
 wallpaper_path=$(swww query | head -n 1 | grep -oP 'image: \K.*')
 
 # Copy the wallpaper to the location Rofi can access
 ln -sf "$wallpaper_path" "$HOME/.config/rofi/.current_wallpaper"
 convert "$wallpaper_path" -resize 40% -filter Gaussian -blur 0x8 $HOME/.config/rofi/.current_wallpaper_blur.png &
-
 
 # execute pywal skipping tty and terminal
 # wal -i $wallpaper_path -s -t
@@ -41,33 +14,6 @@ convert "$wallpaper_path" -resize 40% -filter Gaussian -blur 0x8 $HOME/.config/r
 if [ "$1" != "theme-only" ]; then
     
     hellwal --skip-term-colors -q -i "$wallpaper_path" --static-background "#000000" --static-foreground "#ffffff"
-
-    # # Brightness adjust =========================================================
-    # THRESHOLD=0.5
-
-    # # Use magick to get mean brightness as a float
-    # BRIGHTNESS=$(magick "$wallpaper_path" -colorspace Gray -quiet -format "%[fx:mean]" info: 2>/dev/null)
-
-    # # Remove potential whitespace
-    # BRIGHTNESS="${BRIGHTNESS//[[:space:]]/}"
-
-    # # Compare brightness with threshold
-    # awk -v val="$BRIGHTNESS" -v thresh="$THRESHOLD" '
-    # BEGIN {
-    #     if (val > thresh) {
-    #         exit 0  # Bright
-    #     } else {
-    #         exit 1  # Dark
-    #     }
-    # }
-    # '
-
-    # if [ $? -eq 0 ]; then
-    #     hellwal --skip-term-colors -q -i "$wallpaper_path" --static-background "#000000" --static-foreground "#ffffff"
-    # else
-    #     hellwal --skip-term-colors -b 0.1 -q -i "$wallpaper_path" --static-background "#000000" --static-foreground "#ffffff"
-    # fi
-    # =============================================================================
 else
     hellwal --skip-term-colors --theme ~/.config/hellwal/themes/custom.hellwal
 fi
