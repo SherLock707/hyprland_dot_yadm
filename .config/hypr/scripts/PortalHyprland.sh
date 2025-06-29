320d9301121060378df7796d61d5fdf9ed2724d5
Kiran George <kirangeorge1995@gmail.com>
2025-06-29 14:27:23 +0530

    commit - 2025-06-29_14-27-23

--- .config/hypr/scripts/PortalHyprland.sh
+++ .config/hypr/scripts/PortalHyprland.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 sleep 1
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-hyprland
+# killall xdg-desktop-portal-wlr
+# killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-kde
 killall xdg-desktop-portal
 sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 1
-/usr/lib/xdg-desktop-portal-kde &
-# /usr/libexec/xdg-desktop-portal-hyprland &
+# sleep 1
+# /usr/lib/xdg-desktop-portal-kde &
 sleep 2
-# /usr/lib/xdg-desktop-portal-hyprland &
-/usr/libexec/xdg-desktop-portal &
+/usr/lib/xdg-desktop-portal &
 
