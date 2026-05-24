6840c01d724b70ecf4e03851b014091efe92a150
Kiran George <kirangeorge1995@gmail.com>
2026-05-24 22:47:23 +0530

    Added KDE Connect device info and battery status.

--- .config/waybar/custom_modules/kde_connect_info.sh
+++ .config/waybar/custom_modules/kde_connect_info.sh
@@ -1,37 +1,51 @@
 #!/usr/bin/env bash
 
 PHONE_NAME="POCO F7"
+DEVICE="9f6040691bc544079f3d84a9284d8e67"
 
-DEVICE=$(kdeconnect-cli -a --id-name-only 2>/dev/null | \
-    awk -v phone="$PHONE_NAME" '
-    $0 ~ phone {
-        print $1
-        exit
-    }')
+BASE="/modules/kdeconnect/devices/$DEVICE"
+SERVICE="org.kde.kdeconnect"
 
-if [[ -z "$DEVICE" ]]; then
-    echo '{"text":"disconnected","tooltip":"POCO F7 disconnected"}'
+# Connected?
+CONNECTED=$(qdbus "$SERVICE" "$BASE" \
+org.freedesktop.DBus.Properties.Get \
+org.kde.kdeconnect.device isReachable 2>/dev/null)
+
+[[ "$CONNECTED" != "true" ]] && {
+    echo '{"text":"","alt":"disconnected","tooltip":"POCO F7 disconnected"}'
     exit 0
-fi
+}
 
-BATTERY_PATH="/modules/kdeconnect/devices/$DEVICE/battery"
-DBUS_SERVICE="org.kde.kdeconnect"
-DBUS_IFACE="org.kde.kdeconnect.device.battery"
+# Battery properties in one call
+BATTERY_INFO=$(qdbus "$SERVICE" "$BASE/battery" \
+org.freedesktop.DBus.Properties.GetAll \
+org.kde.kdeconnect.device.battery)
 
-BATTERY=$(qdbus "$DBUS_SERVICE" "$BATTERY_PATH" \
-    org.freedesktop.DBus.Properties.Get \
-    "$DBUS_IFACE" charge)
+BATTERY=$(awk '/charge:/ {print $2}' <<< "$BATTERY_INFO")
+CHARGING=$(awk '/isCharging:/ {print $2}' <<< "$BATTERY_INFO")
 
-CHARGING=$(qdbus "$DBUS_SERVICE" "$BATTERY_PATH" \
-    org.freedesktop.DBus.Properties.Get \
-    "$DBUS_IFACE" isCharging)
+# Notification count
+NOTIFS=$(qdbus "$SERVICE" "$BASE/notifications" \
+org.kde.kdeconnect.device.notifications.activeNotifications 2>/dev/null)
 
+NOTIFS=${NOTIFS:-0}
+
+# State
 STATE="normal"
 
-if [[ "$CHARGING" == "true" ]]; then
-    STATE="charging"
-elif (( BATTERY < 20 )); then
-    STATE="low"
-fi
+[[ "$CHARGING" == "true" ]] && STATE="charging"
+(( BATTERY < 20 && CHARGING != "true" )) && STATE="low"
 
-echo "{\"text\":\"${BATTERY}%\",\"alt\":\"$STATE\",\"tooltip\":\"POCO F7 | ${BATTERY}% | Charging: $CHARGING\"}"
+# Notification badge
+NOTIF_TEXT=""
+(( NOTIFS > 0 )) && \
+NOTIF_TEXT=" <big></big><sup><span foreground='#FAA0A0'></span></sup> $NOTIFS"
+
+printf '{"text":"%s%%%s","alt":"%s","tooltip":" %s\\n󱊣 Battery: %s%%\\n Charging: %s\\n󰎟 Notifications: %s"}\n' \
+"$BATTERY" \
+"$NOTIF_TEXT" \
+"$STATE" \
+"$PHONE_NAME" \
+"$BATTERY" \
+"$CHARGING" \
+"$NOTIFS"
